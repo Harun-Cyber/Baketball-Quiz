7e2c1451910565728b0e6d9a16cc32d6f22ff926
Unknown <unknown@example.com>
2020-01-14 17:44:46 +0100

    Code toegevoegd die ervoor zorgt dat de outputlijst gecombineerd wordt in een HTML-reeks

--- main.js
+++ main.js
@@ -3,13 +3,35 @@ const containResults = document.getElementById("results");
 const submitButton = document.getElementById("submit");
 
 function quizBuilder(){
-    //HTML-uitvoer opslaan
+    //HTML-uitvoer opslaan...
     const output = [];
 
-    //Een for each loop voor elke vraag
-    Questions.forEach(currenQuestion, questionNumber) => {
-        //De lijst met antwoordkeuzes word opgeslagen
-    } 
+    //Een for each loop voor elke vraag...
+    Questions.forEach((currenQuestion, questionNumber) => {
+        //De lijst met antwoordkeuzes word opgeslagen...
+        const answers = [];
+
+        //Voor elk antwoord...
+        for(letter in currenQuestion.answers){
+            
+            //Voeg een button toe...
+            answers.push(
+                `<label>
+                    <input type="radio" name="question${questionNumber}" value="${letter}"></input>
+                    ${letter} :
+                    ${currenQuestion.answers[letter]}
+                </label>`
+            );
+        }
+        //Voeg de antwoorden en vragen naar de output...
+        output.push(
+        `<div class="question">${currenQuestion.question}</div>
+        <div class="answers">${answers.join('')}</div>`
+        );
+    }
+    );
+    //Hier combineer ik de outputlijst in één HTML-reeks en plaats ik het op een pagina...
+    containQuiz.innerHTML = output.join('');
 }
 function resultShower(){
 
